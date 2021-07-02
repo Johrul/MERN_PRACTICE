f9753d09d812bf4454aeb15c89de879faba10efe
johrulit@gmail.com <johrulit@gmail.com>
2021-07-02 09:54:38 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter, Link, Route} from "react-router-dom";
-import Contact from "./pages/Contact";
-import About from "./pages/About";
-import Portfolio from "./pages/Portfolio";
+// import {BrowserRouter, Link, Route} from "react-router-dom";
+// import Contact from "./pages/Contact";
+// import About from "./pages/About";
+// import Portfolio from "./pages/Portfolio";
+import jrouter from "./router/navigation";
 // import jrouter from "./router/router"
 // ReactDOM.render(
 //   <React.StrictMode>
@@ -15,24 +16,7 @@ import Portfolio from "./pages/Portfolio";
 //   document.getElementById('root')
 // );
 
-const jrouter = (
-        <BrowserRouter>
-            <div>
-                <ul>
-                    <Link to={"/"}>Home</Link>
-                    <Link to={"/about"}>About</Link>
-                    <Link to={"/contact"}>Contact</Link>
-                    <Link to={"/portfolio"}>Portfolio</Link>
-                </ul>
 
-                <Route exact path={"/"} component={App}/>
-                <Route path={"/about"} component={About}/>
-                <Route path={"/contact"} component={Contact}/>
-                <Route path={"/portfolio"} component={Portfolio}/>
-
-            </div>
-        </BrowserRouter>
-)
 
 
 
